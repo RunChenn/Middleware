77cd87d05cae71a6a19cb44b825c041156bc75a6
runchen <yma8882003@gmail.com>
2021-07-26 03:10:58 +0800

    edit layout

--- app.js
+++ app.js
@@ -11,23 +11,26 @@ app.set('view engine', 'hbs');
 
 app.use(logRequest);
 
-const logs = []
+const logs = [];
 
 app.get('/', (req, res) => {
     logs.push(res.locals.logs);
-    res.render('index', { logs })
+    res.render('index', { logs });
 })
 
-// app.get('/new', (req, res) => {
-//   res.send('新增 Todo 頁面')
-// })
+app.get('/new', (req, res) => {
+    logs.push(res.locals.logs);
+    res.render('index', { logs });
+})
  
-// app.get('/:id', (req, res) => {
-//   res.send('顯示一筆 Todo')
-// })
+app.get('/:id', (req, res) => {
+    logs.push(res.locals.logs);
+    res.render('index', { logs });
+})
 
 app.post('/', (req, res) => {
- res.render('index')
+    logs.push(res.locals.logs);
+    res.render('index', { logs });
 })
 
 app.listen(port, () => {
--- middleware/logRequest.js
+++ middleware/logRequest.js
@@ -2,14 +2,16 @@ const moment = require('moment');
 const fs = require('fs');
 
 function logRequest(req, res, next) {
+
     const method = req.method;
-    const url = req.url;
+    const url = req._parsedUrl.pathname;
     const date = moment().format('YYYY-MM-DD HH:MM:SS');
+    const logMsg = `${date} | ${method} from /`;
 
     res.locals.logs = { method: method, date: date, url: url };
 
     res.on('finish', () => {
-        fs.appendFile("request_logs.txt", date + "\n", err => {
+        fs.appendFile("request_logs.txt", logMsg + "\n", err => {
             if (err) { console.log(err); }
         });
     });
