dcc7f8e81f24b7d2098c2974e0099baf5e3f6900
runchen <yma8882003@gmail.com>
2021-07-26 00:41:34 +0800

    add handlebars

--- app.js
+++ app.js
@@ -1,25 +1,35 @@
 // app.js
 const express = require('express');
 const exphbs = require('express-handlebars');
+const moment = require('moment');
 const app = express();
 const port = 3000;
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
+app.set('view engine', 'hbs');
+
+let demoLogger = (req, res, next) => {
+    console.log("Hello from logger");
+    next();
+};
+
+app.use(demoLogger);
 
 app.get('/', (req, res) => {
-  res.send('列出全部 Todo')
+//   res.send('列出全部 Todo');
+  res.render('index')
 })
 
-app.get('/new', (req, res) => {
-  res.send('新增 Todo 頁面')
-})
+// app.get('/new', (req, res) => {
+//   res.send('新增 Todo 頁面')
+// })
  
-app.get('/:id', (req, res) => {
-  res.send('顯示一筆 Todo')
-})
+// app.get('/:id', (req, res) => {
+//   res.send('顯示一筆 Todo')
+// })
 
 app.post('/', (req, res) => {
-  res.send('新增一筆  Todo')
+ res.render('index')
 })
 
 app.listen(port, () => {
